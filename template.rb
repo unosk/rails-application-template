82b32914016c1ecf0ea798cf40e90e2ecba41600
Yusuke Shibahara <unosk.s@gmail.com>
2014-05-12 14:36:23 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -73,7 +73,7 @@ EOF
 
 # Replace ERB to Haml
 say_heading "Replace ERR to Haml"
-run "find . -name \*.erb -print | sed 'p;s/.erb$/.haml/' | xargs -n2 html2haml"
+run "find . -name \*.erb -print | sed 'p;s/.erb$/.haml/' | xargs -n2 bundle exec html2haml"
 run "find . -name \*.erb -print | xargs rm"
 
 # Initialize Rspec
