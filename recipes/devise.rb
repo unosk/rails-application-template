1ada6fac16b30ae20450a30c7ff7c6e1791b3ed9
unosk <yusuke.shibahara@heathrow.co.jp>
2015-07-24 13:48:56 +0900

    Deviseの日本語化にdevise-i18nを利用

--- recipes/devise.rb
+++ recipes/devise.rb
@@ -2,6 +2,7 @@ apply File.join(File.dirname(__FILE__), '..', 'helpers.rb') unless defined? TEMP
 
 gem 'devise'
 gem 'devise-encryptable'
+gem 'devise-i18n'
 
 run_bundle
 
@@ -9,7 +10,6 @@ generate 'devise:install'
 
 gsub_file 'config/initializers/devise.rb', /# (config.scoped_views) .*/, '\1 = true'
 
-get 'https://raw.githubusercontent.com/tigrish/devise-i18n/master/config/locales/ja.yml', 'config/locales/devise.ja.yml'
 remove_file 'config/locales/devise.en.yml'
 
 get_from_repo 'spec/support/devise.rb'
